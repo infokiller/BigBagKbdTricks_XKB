74595272ec454361bf7e8c02bd5253caf732f3b8
Yoni Weill <joweill@icloud.com>
2018-05-27 15:03:22 +0300

    Patching the 2016-06-06 data with my Dec 2015 patches.

--- setxkb.sh
+++ setxkb.sh
@@ -40,7 +40,8 @@ X11DIR='/usr/share/X11'; [ -d "${X11DIR}" ] || X11DIR='/usr/lib/X11'
 XKBmodel=pc105caw-sl	# ISO-105 keyboard w/ CurlAngleWide(Slash) mod
 #~ XKBlayout='us(cmk_ed_us),gr(colemak),ru(colemak)'
 XKBlayout='no(cmk_ed_us)'	# Norwegian Colemak[eD]'Universal Symbols' layout
-XKBoption='misc:extend,lv5:caps_switch_lock,grp:shifts_toggle,compose:menu'
+# XKBoption='misc:extend,lv5:caps_switch_lock,grp:shifts_toggle,compose:menu'
+XKBoption='misc:extend,lv5:caps_switch_lock,grp:shifts_toggle'
 VerboseLvl=9			# (-v) How much info should setxkbmap print out?
 KeepXKM='no'			# (-k) Retain old /var/lib/xkb/server-*.xkm files?
 XKBdir="${X11DIR}/xkb"	# (-d) The xkb-type dir to run setxkbmap from
